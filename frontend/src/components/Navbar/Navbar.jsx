a759758d36bc526947e743152b84487386543f79
skrYugadharshini <139324619+skrYugadharshini@users.noreply.github.com>
2024-06-24 11:38:25 +0530

    navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
 import './Navbar.css'
 
+
 const Navbar = () => {
   return (
-    <div className='navbar'>
-        <img src={} alt=''/>
+    <section>
+        <containers>
+   <div className="logo">
+    <h2><i class="ri-pantone-line"></i>E-Learners</h2>
+    <ul className="navbar-menu">
+        <li>Home</li>
+        <li>Menu</li>
+        <li>Contact Us</li>
+        <li>Course Pages</li>
+    </ul>
     </div>
+    </containers>
+    </section>
+
+
+    
+    
   )
 }
 
