57258f68071973c94ce098444a5a7f1d8c2be5f1
skrYugadharshini <139324619+skrYugadharshini@users.noreply.github.com>
2024-07-01 20:01:33 +0530

    route

--- Online-Educational-Website-main/src/App.js
+++ Online-Educational-Website-main/src/App.js
@@ -1,36 +1,45 @@
 
 import Home from "./pages/Home";
-import { Form } from "reactstrap";
-import AboutUs from "./components/About-us/AboutUs";
-
-
-
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import ChooseUs from "./components/Choose-us/ChooseUs";
-
 
 function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-      <ul>
-        <li><link to='/'>AboutUs</link></li>
-      </ul>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/About-us" element={<AboutUs/>}/>
-        <Route path="/Choose-us" element={<ChooseUs/>}/>
-
-
-      
-      </Routes>
-
-      </BrowserRouter>
-    </div>
-  );
-  
-        
-
+  return <Home />
+;
 }
 
 export default App;
+
+
+//import AboutUs from "./components/About-us/AboutUs";
+
+
+
+//import {BrowserRouter, Routes, Route} from 'react-router-dom';
+//import ChooseUs from "./components/Choose-us/ChooseUs";
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//      { <BrowserRouter>
+//       <ul>
+//         <li><link to='/'>AboutUs</link></li>
+//       </ul>
+//       <Routes>
+//         <Route path="/" element={<Home/>}/>
+//         <Route path="/About-us" element={<AboutUs/>}/>
+//         <Route path="/Choose-us" element={<ChooseUs/>}/>
+
+
+      
+//       </Routes>
+
+//       </BrowserRouter>
+//       </div>
+// }
+//   );
+    
+
+
+
+
+
