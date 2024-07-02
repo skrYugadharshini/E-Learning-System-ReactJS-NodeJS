948e82323b174d18f5dd6ab98805644ad70b4469
skrYugadharshini <139324619+skrYugadharshini@users.noreply.github.com>
2024-07-02 20:18:30 +0530

    route

--- Online-Educational-Website-main/src/App.js
+++ Online-Educational-Website-main/src/App.js
@@ -1,56 +1,7 @@
-// import Home from "./pages/Home";
-// import {Link} from 'react-router-dom';
-// import AboutUs from "./components/About-us/AboutUs";
-// import {BrowserRouter, Routes, Route} from 'react-router-dom';
-// import ChooseUs from "./components/Choose-us/ChooseUs";
-
-
-// function App() {
-//   return (
-//     <div className="App">
-//      { <BrowserRouter>
-//       <ul>
-//         <li><link to='/'>AboutUs</link></li>
-//       </ul>
-//       <Routes>
-//         <Route path="/" element={<Home/>}/>
-//         <Route path="/About-us" element={<AboutUs/>}/>
-//         <Route path="/Choose-us" element={<ChooseUs/>}/>
-//         </Routes>
-
-//       </BrowserRouter>
-//       </div>
-// }
-//   );
-
-import {Routers, Route} from 'react-router-dom'
-//import { Navbar } from 'reactstrap'
-import AboutUs from './components/About-us/AboutUs'
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import ChooseUs from './components/Choose-us/ChooseUs';
+import Home from "./pages/Home";
 
 function App() {
-  return(
-    
-    <div className="App">
-     { <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/' element={<AboutUs/>}/>
-      <Route path='/' element={<ChooseUs/>}/>
-    </Routes>
-    </BrowserRouter>
-    </div>}
-     
-
-  )
+  return <Home />;
 }
 
-
-
-    
-
-
-
-
-
+export default App;
