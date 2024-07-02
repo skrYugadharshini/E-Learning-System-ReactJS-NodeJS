2e9c1fa3c4d5fa843ff902f43cc18852bf299cec
skrYugadharshini <139324619+skrYugadharshini@users.noreply.github.com>
2024-07-02 09:19:30 +0530

    route

--- Online-Educational-Website-main/src/App.js
+++ Online-Educational-Website-main/src/App.js
@@ -1,42 +1,30 @@
-
 import Home from "./pages/Home";
+import {Link} from 'react-router-dom';
+import AboutUs from "./components/About-us/AboutUs";
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import ChooseUs from "./components/Choose-us/ChooseUs";
+
 
 function App() {
-  return <Home />
-;
+  return (
+    <div className="App">
+     { <BrowserRouter>
+      <ul>
+        <li><link to='/'>AboutUs</link></li>
+      </ul>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/About-us" element={<AboutUs/>}/>
+        <Route path="/Choose-us" element={<ChooseUs/>}/>
+        </Routes>
+
+      </BrowserRouter>
+      </div>
 }
-
-export default App;
-
-
-//import AboutUs from "./components/About-us/AboutUs";
+  );
 
 
 
-//import {BrowserRouter, Routes, Route} from 'react-router-dom';
-//import ChooseUs from "./components/Choose-us/ChooseUs";
-
-
-// function App() {
-//   return (
-//     <div className="App">
-//      { <BrowserRouter>
-//       <ul>
-//         <li><link to='/'>AboutUs</link></li>
-//       </ul>
-//       <Routes>
-//         <Route path="/" element={<Home/>}/>
-//         <Route path="/About-us" element={<AboutUs/>}/>
-//         <Route path="/Choose-us" element={<ChooseUs/>}/>
-
-
-      
-//       </Routes>
-
-//       </BrowserRouter>
-//       </div>
-// }
-//   );
     
 
 
