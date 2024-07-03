e10d3173612e2e89ad858092b1f689f7410fc1d5
skrYugadharshini <139324619+skrYugadharshini@users.noreply.github.com>
2024-07-03 09:47:24 +0530

    route

--- Online-Educational-Website-main/src/components/Header/Header.jsx
+++ Online-Educational-Website-main/src/components/Header/Header.jsx
@@ -1,80 +1,103 @@
-import React, { useRef } from "react";
-import { Container } from "reactstrap";
-import "./header.css";
+// import React, { useRef } from "react";
+// import { Container } from "reactstrap";
+// import "./header.css";
 
 
-<header class="header"></header>
-const navLinks = [
-  {
-    display: "Home",
-    url: "#",
-  },
-  {
-    display: "About Us",
-    url: "#",
-  },
+// <header class="header"></header>
+// const navLinks = [
+//   {
+//     display: "Home",
+//     url: "#",
+//   },
+//   {
+//     display: "About Us",
+//     url: "#",
+//   },
 
-  {
-    display: "Courses",
-    url: "#",
-  },
-  {
-    display: "Academy",
-    url: "#",
-  },
-  {
-    display: "Certification",
-    url: "#",
-  },
-];
+//   {
+//     display: "Courses",
+//     url: "#",
+//   },
+//   {
+//     display: "Academy",
+//     url: "#",
+//   },
+//   {
+//     display: "Certification",
+//     url: "#",
+//   },
+// ];
 
-const Header = () => {
-  const menuRef = useRef();
+// const Header = () => {
+//   const menuRef = useRef();
 
-  const menuToggle = () => menuRef.current.classList.toggle("active__menu");
+//   const menuToggle = () => menuRef.current.classList.toggle("active__menu");
 
-  return (
-    <header className="header">
-      <Container>
-        <div className="navigation d-flex align-items-center justify-content-between">
-          <div className="logo">
-            <h2 className=" d-flex align-items-center gap-1">
-              <i class="ri-pantone-line"></i> <span>Bright</span>learn
-            </h2>
-          </div>
+//   return (
+//     <header className="header">
+//       <Container>
+//         <div className="navigation d-flex align-items-center justify-content-between">
+//           <div className="logo">
+//             <h2 className=" d-flex align-items-center gap-1">
+//               <i class="ri-pantone-line"></i> <span>Bright</span>learn
+//             </h2>
+//           </div>
 
-          <div className="nav d-flex align-items-center gap-5">
-            <div className="nav__menu" ref={menuRef} onClick={menuToggle}>
-              <ul className="nav__list">
-                {navLinks.map((item, index) => (
-                  <li key={index} className="nav__item">
-                    <a href={item.url}>{item.display}</a>
-                  </li>
-                ))}
-              </ul>
-            </div>
+//           <div className="nav d-flex align-items-center gap-5">
+//             <div className="nav__menu" ref={menuRef} onClick={menuToggle}>
+//               <ul className="nav__list">
+//                 {navLinks.map((item, index) => (
+//                   <li key={index} className="nav__item">
+//                     <a href={item.url}>{item.display}</a>
+//                   </li>
+//                 ))}
+//               </ul>
+//             </div>
 
-<div>
+// <div>
 
-            <button className="btn free__btn">Sign in</button>
+//             <button className="btn free__btn">Sign in</button>
             
 
-          </div>
-        </div>
-</div>
-        <div className="mobile__menu">
-          <span>
-            <i class="ri-menu-line" onClick={menuToggle}></i>
+//           </div>
+//         </div>
+// </div>
+//         <div className="mobile__menu">
+//           <span>
+//             <i class="ri-menu-line" onClick={menuToggle}></i>
 
             
-          </span>
-          </div>
+//           </span>
+//           </div>
 
       
 
-      </Container>
-    </header>
-  );
-};
+//       </Container>
+//     </header>
+//   );
+// };
 
-export default Header;
+// export default Header;
+
+import React from 'react'
+import "./header.css"
+
+
+
+const header = () => {
+  return(
+    <div className='header-menu'>
+      <ul>
+        <li>Home</li>
+        <li>Course</li>
+        <li>Academy</li>
+        <li>Certificate</li>
+      </ul>
+
+      
+
+      </div>
+
+  
+  )
+}
