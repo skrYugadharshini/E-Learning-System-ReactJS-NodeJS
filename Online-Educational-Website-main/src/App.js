64f716d067b4e2709e2525bea9ee4c06d659ba4e
skrYugadharshini <139324619+skrYugadharshini@users.noreply.github.com>
2024-07-03 09:11:25 +0530

    route

--- Online-Educational-Website-main/src/App.js
+++ Online-Educational-Website-main/src/App.js
@@ -1,21 +1,6 @@
-import { Card, Placeholder } from "reactstrap";
 import Home from "./pages/Home";
-import { Route, Routes } from "react-router-dom";
-import Cart from './components/Cart/Cart'
 
-const App =() =>
-  return (
-    <>
-    <div className='app'>
-      <header/>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/Cart' element={<Card/>}/>
-        <Route path='/order' element={<Placeholder}/>
-      </Routes>
-    </div>
-    </>
-  )
+
 function App() {
   return <Home />;
 }
