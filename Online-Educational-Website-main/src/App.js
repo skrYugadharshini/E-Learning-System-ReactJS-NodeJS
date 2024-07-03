4f1041f7dbec63f9b8278a37cc0587dafc3bce58
skrYugadharshini <139324619+skrYugadharshini@users.noreply.github.com>
2024-07-03 08:25:58 +0530

    route

--- Online-Educational-Website-main/src/App.js
+++ Online-Educational-Website-main/src/App.js
@@ -1,6 +1,18 @@
 import Home from "./pages/Home";
+import { Route, Routes } from "react-router-dom";
 
 
+const App =() =>
+  return (
+    <>
+    <div className='app'>
+      <header/>
+      <Routes>
+        <Route path='/' element={<Home/>}
+      </Routes>
+    </div>
+    </>
+  )
 function App() {
   return <Home />;
 }
