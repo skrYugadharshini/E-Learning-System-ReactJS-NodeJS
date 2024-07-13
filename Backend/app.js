84d7f8e184f4d3bc3f2745175957fbea02824d12
skrYugadharshini <139324619+skrYugadharshini@users.noreply.github.com>
2024-07-13 17:45:10 +0530

    backend

---
+++ Backend/app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const dotenv - require('dotenv')
+
+
+app.listen(8000,() =>{
+    console.log('server listening to port 8000 in production')
+})
--- Backend/config/db.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async () =>{
-    (await mongoose.connect('mongodb+srv://chayugadharshini:<password>@yuga.pom4bwg.mongodb.net/e-learnig')).then(()=>console.log("DB Connected"));
-}
