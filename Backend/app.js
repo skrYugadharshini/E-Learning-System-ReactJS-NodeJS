64df59ab52fe90b3f1f2b30fe84b0c6b5ca59b9d
skrYugadharshini <139324619+skrYugadharshini@users.noreply.github.com>
2024-10-14 11:54:16 +0530

    basic

--- Backend/app.js
+++ Backend/app.js
@@ -213,3 +213,64 @@ app.use('/api/v1' ,order);
 app.listen(process.env.PORT, () => {
     console.log('Server listening to Port ${process.env.PORT} in ${process.env.NODE_ENV');
 });
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const path = require('path');
+dotenv.config({path: path.join(__dirname, 'config','config.env')})
+
+const products = require('./routes/product');
+const order = require('./route/order');
+
+app.use('/api/v1/',products);
+app.use('/api/v1' ,order);
+
+
+app.listen(process.env.PORT, () => {
+    console.log('Server listening to Port ${process.env.PORT} in ${process.env.NODE_ENV');
+});const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const path = require('path');
+dotenv.config({path: path.join(__dirname, 'config','config.env')})
+
+const products = require('./routes/product');
+const order = require('./route/order');
+
+app.use('/api/v1/',products);
+app.use('/api/v1' ,order);
+
+
+app.listen(process.env.PORT, () => {
+    console.log('Server listening to Port ${process.env.PORT} in ${process.env.NODE_ENV');
+});const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const path = require('path');
+dotenv.config({path: path.join(__dirname, 'config','config.env')})
+
+const products = require('./routes/product');
+const order = require('./route/order');
+
+app.use('/api/v1/',products);
+app.use('/api/v1' ,order);
+
+
+app.listen(process.env.PORT, () => {
+    console.log('Server listening to Port ${process.env.PORT} in ${process.env.NODE_ENV');
+});const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const path = require('path');
+dotenv.config({path: path.join(__dirname, 'config','config.env')})
+
+const products = require('./routes/product');
+const order = require('./route/order');
+
+app.use('/api/v1/',products);
+app.use('/api/v1' ,order);
+
+
+app.listen(process.env.PORT, () => {
+    console.log('Server listening to Port ${process.env.PORT} in ${process.env.NODE_ENV');
+});
