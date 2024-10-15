1540af4ddc95884658a238d4d6fc0834776f65c0
skrYugadharshini <139324619+skrYugadharshini@users.noreply.github.com>
2024-10-15 18:24:55 +0530

    basic

--- Backend/app.js
+++ Backend/app.js
@@ -148,3 +148,27 @@ app.use('/api/v1' ,order);
 
 app.listen(process.env.PORT, () => {
     console.log('Server listening to Port ${process.env.PORT} in ${process.env.NODE_ENV');
+    dotenv.config({path: path.join(__dirname, 'config','config.env')})
+
+    app.listen(process.env.PORT, () => {
+        console.log('Server listening to Port ${process.env.PORT} in ${process.env.NODE_ENV');
+        const express = require('express');
+    const app = express();
+    const dotenv = require('dotenv');
+    const path = require('path');
+    dotenv.config({path: path.join(__dirname, 'config','config.env')})
+    
+    const products = require('./routes/product');
+    const order = require('./route/order');
+    
+    app.use('/api/v1/',products);
+    app.use('/api/v1' ,order);
+    
+    
+    app.listen(process.env.PORT, () => {
+        console.log('Server listening to Port ${process.env.PORT} in ${process.env.NODE_ENV');
+    });const express = require('express');
+    const app = express();
+    const dotenv = require('dotenv');
+    const path = require('path');
+    dotenv.config({path: path.join(__dirname, 'config','config.env')})
