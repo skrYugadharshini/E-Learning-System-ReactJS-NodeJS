fa8d8c1b32ecf76a03e47be110654a2d3031efa5
skrYugadharshini <139324619+skrYugadharshini@users.noreply.github.com>
2024-10-21 19:02:45 +0530

    basic

--- Backend/app.js
+++ Backend/app.js
@@ -1925,3 +1925,49 @@ const app = express();
 const dotenv = require('dotenv');
 const path = require('path');
 dotenv.config({path: path.join(__dirname, 'config','config.env')})
+const path = require('path');
+dotenv.config({path: path.join(__dirname, 'config','config.env')})dotenv.config({path: path.join(__dirname, 'config','config.env')})app.listen(process.env.PORT, () => {
+    console.log('Server listening to Port ${process.env.PORT} in ${process.env.NODE_ENV');
+    const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const path = require('path');
+dotenv.config({path: path.join(__dirname, 'config','config.env')})
+
+const products = require('./routes/product');
+const order = require('./route/order');
+
+app.use('/api/v1/',products);
+app.use('/api/v1' ,order);
+
+
+app.listen(process.env.PORT, () => {
+    console.log('Server listening to Port ${process.env.PORT} in ${process.env.NODE_ENV');
+});const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const path = require('path');
+dotenv.config({path: path.join(__dirname, 'config','config.env')})const path = require('path');
+dotenv.config({path: path.join(__dirname, 'config','config.env')})dotenv.config({path: path.join(__dirname, 'config','config.env')})app.listen(process.env.PORT, () => {
+    console.log('Server listening to Port ${process.env.PORT} in ${process.env.NODE_ENV');
+    const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const path = require('path');
+dotenv.config({path: path.join(__dirname, 'config','config.env')})
+
+const products = require('./routes/product');
+const order = require('./route/order');
+
+app.use('/api/v1/',products);
+app.use('/api/v1' ,order);
+
+
+app.listen(process.env.PORT, () => {
+    console.log('Server listening to Port ${process.env.PORT} in ${process.env.NODE_ENV');
+});const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const path = require('path');
+dotenv.config({path: path.join(__dirname, 'config','config.env')})
+dotenv.config({path: path.join(__dirname, 'config','config.env')})
