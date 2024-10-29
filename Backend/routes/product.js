ac26ea84c81b825bfcf0f3d4b833392b48d237c8
skrYugadharshini <139324619+skrYugadharshini@users.noreply.github.com>
2024-10-29 18:45:39 +0530

    basic

--- Backend/routes/product.js
+++ Backend/routes/product.js
@@ -102,3 +102,25 @@ router.route('/products').get(getProducts);
 router.route('/products/:id').get(getSingleProducts);
 
 module.exports = router;
+const express = require('express');
+const { getProducts, getSingleProducts } = require('../controllers/productController');
+const router = express.Router();
+
+router.route('/products').get(getProducts);
+router.route('/products/:id').get(getSingleProducts);
+
+module.exports = router;const express = require('express');
+const { getProducts, getSingleProducts } = require('../controllers/productController');
+const router = express.Router();
+
+router.route('/products').get(getProducts);
+router.route('/products/:id').get(getSingleProducts);
+
+module.exports = router;const express = require('express');
+const { getProducts, getSingleProducts } = require('../controllers/productController');
+const router = express.Router();
+
+router.route('/products').get(getProducts);
+router.route('/products/:id').get(getSingleProducts);
+
+module.exports = router;
